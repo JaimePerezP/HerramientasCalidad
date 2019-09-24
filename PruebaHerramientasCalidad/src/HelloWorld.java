ac3007502b722efc26c8e443b2f68c40846bb191
JaimePerezP <43539902+JaimePerezP@users.noreply.github.com>
2019-09-24 10:32:25 +0200

    Mayusculas

--- PruebaHerramientasCalidad/src/HelloWorld.java
+++ PruebaHerramientasCalidad/src/HelloWorld.java
@@ -2,7 +2,12 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int A = 1;
+		int A = COMPADRE();
+	}
+	
+	public static int COMPADRE() {
+		int SI = 0;
+		return SI;
 	}
 
 }
