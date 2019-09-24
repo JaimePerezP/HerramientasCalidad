61ca3f19b8adbdab4cfe423b8c4e49f365da40e3
JaimePerezP <43539902+JaimePerezP@users.noreply.github.com>
2019-09-24 10:35:43 +0200

    Hola mundo

--- PruebaHerramientasCalidad/src/HelloWorld.java
+++ PruebaHerramientasCalidad/src/HelloWorld.java
@@ -2,12 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int A = COMPADRE();
-	}
-	
-	public static int COMPADRE() {
-		int SI = 0;
-		return SI;
+		System.out.println("Hola Mundo!");
 	}
 
 }
